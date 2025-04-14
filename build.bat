c3997cece53124c81c7364cb2db2a9ff03279755
Vitamin4107 <Vitaminoznik@yandex.ru>
2025-04-14 02:10:46 +0200

    New variable VSDEV_PATH for determinate path to developer console

--- build.bat
+++ build.bat
@@ -2,75 +2,69 @@
 SETLOCAL EnableDelayedExpansion
 
 IF NOT EXIST "build_config.ini" (
-    echo Error: build_config.ini not found.
-    goto :EOF
+    ECHO Error: build_config.ini not found.
+    GOTO :EOF
 )
 
 FOR /F "tokens=1* delims==" %%L IN ('type build_config.ini ^| find "="') DO (
     SET "%%L=%%M"
 )
 
+IF NOT DEFINED VSINSTALLDIR (
+    IF DEFINED VSDEV_PATH (
+         CALL SET "VSDEV_CMD=!VSDEV_PATH!"
+    )
+
+    IF NOT EXIST "!VSDEV_CMD!" (
+         ECHO Error: vsdevcmd.bat not found at "!VSDEV_CMD!"
+         GOTO :EOF
+    ) ELSE (
+         CALL "!VSDEV_CMD!" -arch=x64
+    )
+)
+
 SET "METAMOD_PATH=%BASE_PATH%\metamod"
 SET "SOURCE2_PATH=%BASE_PATH%\Source2"
 SET "PLUGIN_PATH=%BASE_PATH%\%OUTPUT_NAME%"
 
-if not exist "%PLUGIN_PATH%\xbyak\.git" (
-    echo Cloning xbyak submodule from GitHub...
-    %GIT_CMD% clone -b %XBYAK_BRANCH% %XBYAK_REPO% "%PLUGIN_PATH%\xbyak"
-    IF ERRORLEVEL 1 (
-       echo Failed to clone xbyak repository.
-       goto :EOF
-    )
-) else (
-    echo Updating xbyak submodule...
-    pushd "%PLUGIN_PATH%\xbyak"
-    %GIT_CMD% pull
-    IF ERRORLEVEL 1 (
-         echo Failed to update xbyak repository.
-         popd
-         goto :EOF
-    )
-    popd
-)
-
 (
-    echo /nologo
-    echo /O1
-    echo /GL
-    echo /std:c++17
-    echo /MT
-    echo /W4
-    echo /EHsc
-    echo /D_CRT_SECURE_NO_WARNINGS
-    echo /DMETA_IS_SOURCE2
-    echo /DMETA_NO_HL2SDK
-    echo /I"%METAMOD_PATH%"
-    echo /I"%METAMOD_PATH%\core"
-    echo /I"%METAMOD_PATH%\core\sourcehook"
-    echo /I"%SOURCE2_PATH%"
-    echo /I"%PLUGIN_PATH%"
-    echo /I"%PLUGIN_PATH%\xbyak"
-    echo /c
-    echo "%METAMOD_PATH%\core\metamod_oslink.cpp"
-    echo "%PLUGIN_PATH%\utils\detour_manager.cpp"
-    echo "%PLUGIN_PATH%\%PLUGIN_NAME%.cpp"
+    ECHO /nologo
+    ECHO /O1
+    ECHO /GL
+    ECHO /std:c++17
+    ECHO /MT
+    ECHO /W4
+    ECHO /EHsc
+    ECHO /D_CRT_SECURE_NO_WARNINGS
+    ECHO /DMETA_IS_SOURCE2
+    ECHO /DMETA_NO_HL2SDK
+    ECHO /I"%METAMOD_PATH%"
+    ECHO /I"%METAMOD_PATH%\core"
+    ECHO /I"%METAMOD_PATH%\core\sourcehook"
+    ECHO /I"%SOURCE2_PATH%"
+    ECHO /I"%PLUGIN_PATH%"
+    ECHO /I"%PLUGIN_PATH%\xbyak"
+    ECHO /c
+    ECHO "%METAMOD_PATH%\core\metamod_oslink.cpp"
+    ECHO "%PLUGIN_PATH%\utils\detour_manager.cpp"
+    ECHO "%PLUGIN_PATH%\%PLUGIN_NAME%.cpp"
 ) > compile.rsp
 
 %RC_CMD% /nologo /fo version.res metadata\version.rc
 IF ERRORLEVEL 1 (
-    echo Failed to compile the resource file.
+    ECHO Failed to compile the resource file.
     goto :EOF
 )
 
 %CL_CMD% @compile.rsp
 IF ERRORLEVEL 1 (
-    echo Compilation failed.
+    ECHO Compilation failed.
     goto :EOF
 )
 
 %LINK_CMD% %LINKER_PARAM% metamod_oslink.obj %PLUGIN_NAME%.obj detour_manager.obj version.res /OUT:%OUTPUT_NAME%.dll
 IF ERRORLEVEL 1 (
-    echo Linking failed.
+    ECHO Linking failed.
     goto :EOF
 )
 
@@ -82,5 +76,5 @@ if EXIST *.obj del *.obj
 if EXIST version.res del version.res
 if EXIST compile.rsp del compile.rsp
 
-echo Build completed successfully.
+ECHO Build completed successfully.
 ENDLOCAL
